ee0b440c5f2756a28125b4b9210525907330ae62
Diego <diego.lavezzari@gmail.com>
2024-12-15 21:35:39 -0300

    ajuste de variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,22 @@
-variable "mi_subnet" {
-  description = "Lista subnet"
-  type = list(string)
-}
-
-variable "app_replicas" {
-  description = "2"
-  type        = number
-  default     = 2
-}
-
 variable "vpc_id" {
-  description = "vpc-0889bae2746549098"
+  description = "vpc-eks-vote"
   type        = string
+  default     = "vpc-07501a4fd9aabe6b5"
+}
+
+variable "mi_subnet" {
+  description = "subnet-eks-vote"
+  type        = list(string)
+  default     = [
+    "subnet-0dc31742c5e7303b2", # Private subnet 1
+    "subnet-073466fb88c926542", # Public subnet 1
+    "subnet-022424d38893f82e2", # Private subnet 2
+    "subnet-0804dd61a2ab23cbe"  # Public subnet 2
+  ]
 }
 
 variable "security_group_ids" {
-  description = "sg-003a42ddbcb9cf824"
+  description = "sg-eks-vote"
   type        = list(string)
 }
 
@@ -42,3 +43,8 @@ variable "max_size" {
   default     = 4
 }
 
+variable "app_replicas" {
+  description = "2"
+  type        = number
+  default     = 2
+}
